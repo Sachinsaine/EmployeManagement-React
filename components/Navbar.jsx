d677f85547fac0510760304f9b52bddc571b8666
Ronnie <royalronnie05@gmail.com>
2026-07-15 18:04:04 +0530

    first commit

---
+++ components/Navbar.jsx
@@ -0,0 +1,12 @@
+import styles from "./Navbar.module.css";
+export const Navbar = () => {
+  return (
+    <div className={styles.navCont}>
+      <div>
+        <h1 className={styles.logo}>Employees</h1>
+        <div>Manage your team's records in one place.</div>
+      </div>
+      <button className={styles.addBtn}>+ Add Employe</button>
+    </div>
+  );
+};
