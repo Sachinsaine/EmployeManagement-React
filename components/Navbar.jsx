02c86effa8f2126144d0359ad7155980d90e5b8c
Ronnie <royalronnie05@gmail.com>
2026-07-19 16:42:11 +0530

    almost done

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,13 +1,17 @@
 import styles from "./Navbar.module.css";
+
 export const Navbar = ({ handleOpen }) => {
   return (
     <div className={styles.navCont}>
       <div>
         <h1 className={styles.logo}>Employees</h1>
-        <div>Manage your team's records in one place.</div>
+        <div className={styles.subTitle}>
+          Manage your team's records in one place.
+        </div>
       </div>
+
       <button onClick={handleOpen} className={styles.addBtn}>
-        + Add Employe
+        + Add Employee
       </button>
     </div>
   );
